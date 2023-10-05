d0f50c3fd453c1488775d73e22d5eb9c43587385
Tekurukichi <ovnudga.fitm22@kubg.edu.ua>
2023-10-05 14:52:47 +0300

    first commit

---
+++ Lab1/Lab1_01/Address.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ConsoleApp1
+{
+    public class Address
+    {
+        public string index { get; set; }
+        public string country { get; set; }
+        public string city { get; set; }
+        public string street { get; set; }
+        public string house { get; set; }
+        public string apartment { get; set; }
+        
+        
+        
+         
+        
+
+
+    }
+}
---
+++ Lab1/Lab1_01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Address MyAddress = new Address();
+            MyAddress.index = "0524695";
+            MyAddress.country = "Ukraine";
+            MyAddress.city = "Kyiv";
+            MyAddress.street = "Obl Prosp";
+            MyAddress.house = "12a";
+            MyAddress.apartment = "34";
+            Console.WriteLine($"Адреса: {MyAddress.country}, {MyAddress.street}, {MyAddress.city}," +
+                              $"{MyAddress.house}, {MyAddress.apartment}, {MyAddress.index}");
+
+
+            
+
+        }
+    }
+}
